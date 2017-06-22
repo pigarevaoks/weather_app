fe0e91a2ac1501f4687edda92b244f884ddbba42
Pigareva Oksana <pigarevaoks@gmail.com>
2017-06-22 22:02:11 +0300

    Fix api request

--- source/components/Search/Search.js
+++ source/components/Search/Search.js
@@ -6,10 +6,27 @@ export default class Search extends Component {
 		super(props);
 		this.handleSearch = this.handleSearch.bind(this);
 	}
+	
 	handleSearch(event) {
+		const domen = 'api.openweathermap.org/data/2.5/weather';
+		const key = '63d0d4d4fbe038a5fbb63e0b3bc3e243';
+
 		let cityName = event.target.value;
 		if (cityName.length < 3) { return false }
+
+		fetch(`http://${domen}?q=${cityName}&appid=${key}`)
+			.then((response) => response.json())
+			.then((response) => {
+				this.props.updateInfo({
+					temp: response.main.temp,
+					name: response.name
+				})
+			})
+			.catch((error) => {
+				console.error(error);
+			})
 	}
+
 	render() {
 		return (
 			<div className="search">
